91fc04af38392fd200088eaf46c0b15586e56476
Etcetera <yilonglei790@gmail.com>
2022-10-04 16:00:40 +0800

    改改

--- 2.通用组合函数.js
+++ 2.通用组合函数.js
@@ -1,29 +1,29 @@
-function hyCompose(...fns) {
-    var length = fns.length
-    for (var i = 0; i < length; i++) {
-        if (typeof fns[i] !== 'function') {
-            throw new TypeError("Expected arguments are functions")
-        }
+function etcCompose(...fns) {
+  var length = fns.length
+  for (var i = 0; i < length; i++) {
+    if (typeof fns[i] !== "function") {
+      throw new TypeError("Expected arguments are functions")
     }
+  }
 
-    function compose(...args) {
-        var index = 0
-        var result = length ? fns[index].apply(this, args) : args
-        while (++index < length) {
-            result = fns[index].call(this, result)
-        }
-        return result
+  function compose(...args) {
+    var index = 0
+    var result = length ? fns[index].apply(this, args) : args
+    while (++index < length) {
+      result = fns[index].call(this, result)
     }
-    return compose
+    return result
+  }
+  return compose
 }
 
 function double(m) {
-    return m * 2
+  return m * 2
 }
 
 function square(n) {
-    return n ** 2
+  return n ** 2
 }
 
-var newFn = hyCompose(double, square)
+var newFn = etcCompose(double, square)
 console.log(newFn(10))
--- 3.手写Promise.js
+++ 3.手写Promise.js
@@ -1,36 +1,36 @@
-const PROMISE_STATUS_PENDING = 'pending'
-const PROMISE_STATUS_FULFILLED = 'fulfilled'
-const PROMISE_STATUS_REJECTED = 'rejected'
+const PROMISE_STATUS_PENDING = "pending"
+const PROMISE_STATUS_FULFILLED = "fulfilled"
+const PROMISE_STATUS_REJECTED = "rejected"
 
 class EtcPromise {
-    constructor(executor) {
-        this.status = PROMISE_STATUS_PENDING
-        this.value = undefined
-        this.reason = undefined
+  constructor(executor) {
+    this.status = PROMISE_STATUS_PENDING
+    this.value = undefined
+    this.reason = undefined
 
-        const resolve = (value) => {
-            if (this.status === PROMISE_STATUS_PENDING) {
-                this.status = PROMISE_STATUS_FULFILLED
-                this.value = value
-                console.log("resolve被调用")
-            }
-        }
-
-        const reject = (reason) => {
-            if (this.status === PROMISE_STATUS_PENDING) {
-                this.status = PROMISE_STATUS_REJECTED
-                this.reason = reason
-                console.log("reject被调用")
-            }
-        }
-
-        executor(resolve, reject)
+    const resolve = (value) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_FULFILLED
+        this.value = value
+        console.log("resolve被调用")
+      }
     }
+
+    const reject = (reason) => {
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.status = PROMISE_STATUS_REJECTED
+        this.reason = reason
+        console.log("reject被调用")
+      }
+    }
+
+    executor(resolve, reject)
+  }
 }
 
 const promise = new EtcPromise((resolve, reject) => {
-    console.log("状态pending")
+  console.log("状态pending")
 
-    resolve(1111)
-    reject(2222)
+  resolve(1111)
+  reject(2222)
 })
