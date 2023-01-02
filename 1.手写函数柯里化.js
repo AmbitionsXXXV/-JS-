b1a2ecd26d476889f9b07799c71432c10f955301
Etcetera <yilonglei790@gmail.com>
2023-01-02 14:38:56 +0800

    自动化柯里化修改

--- 1.手写函数柯里化.js
+++ 1.手写函数柯里化.js
@@ -2,14 +2,16 @@ function etcCurrying(fn) {
   function curried(...args) {
     // 判断当前已经接收的参数的个数，可以参数本身需要接收的参数是否一致
     if (args.length >= fn.length) {
-      // fn(...args)
+      //return fn(...args)
       // fn.apply(this, ...args)
+      // 绑定有this
       return fn.apply(this, args)
     } else {
       // 没有达到个数时, 需要返回一个新的函数, 继续来接收的参数
       function curried2(...args2) {
         // 接收到参数后, 需要递归调用curried来检查函数的个数是否达到
-        return curried.apply(this, args.concat)
+        // 考虑this情况
+        return curried.apply(this, args.concat(args2));
       }
       return curried2
     }
