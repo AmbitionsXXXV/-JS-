2a57e3071ff938df6ecb90be62d19924db1856ce
Etcetera <yilonglei790@gmail.com>
2023-01-08 21:49:42 +0800

    优化个人实现的bind函数

--- 8.手写bind.js
+++ 8.手写bind.js
@@ -2,21 +2,20 @@ function foo(name, age) {
   console.log(this, name, age)
 }
 
-Function.prototype.etcBind = function (thisArgs, ...otherArgs) {
-  thisArgs = thisArgs === null || thisArgs === undefined ? window : Object(thisArgs)
-  Object.defineProperty(thisArgs, "fn", {
+Function.prototype.etcBind = function (thisArg, ...otherArgs) {
+  thisArg = thisArg == null ? globalThis : Object(thisArg)
+  Object.defineProperty(thisArg, 'fn', {
     enumerable: false,
     configurable: true,
     writable: false,
-    value: this,
+    value: this
   })
-  // console.log(this)
+
   return (...newArgs) => {
-    // var allArgs = otherArgs.concat(newArgs)
     var allArgs = [...otherArgs, ...newArgs]
-    thisArgs.fn(...allArgs)
+    thisArg.fn(...allArgs)
   }
 }
 
-var newFoo = foo.etcBind({ name: "oor" })
+var newFoo = foo.etcBind({ name: 'oor' })
 newFoo()
