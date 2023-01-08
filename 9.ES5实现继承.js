b296aec6f1322a758de893741de114bd34ea2390
Etcetera <yilonglei790@gmail.com>
2023-01-08 20:38:37 +0800

    使用ES5特性实现继承

--- 9.ES5实现继承.js
+++ 9.ES5实现继承.js
@@ -11,12 +11,13 @@ function createObject(obj) {
 }
 
 // 寄生组合式函数
-function createInfo(obj, name, age, height, address) {
-  var newObj = createObject(obj)
-  newObj.name = name
-  newObj.age = age
-  newObj.height = height
-  newObj.address = address
-
-  return newObj
+function inherit(Subtype, Supertype) {
+  Subtype.prototype = createObject(Supertype.prototype)
+  Object.defineProperty(Subtype.prototype, 'constructor', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: Subtype
+  })
+  Object.setPrototypeOf(Subtype, Supertype)
 }
