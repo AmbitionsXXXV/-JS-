12413acbb67c60ba5cf84202eac8e706cabfd79c
Etcetera <yilonglei790@gmail.com>
2023-01-10 21:57:30 +0800

    立即执行功能

--- 10.实现防抖.js
+++ 10.实现防抖.js
@@ -1,18 +1,28 @@
-function etcDebounce(fn, delay) {
+function etcDebounce(fn, delay, immediate = false) {
   let timer = null
+  let isExec = false
 
   const _debounce = function (...args) {
     if (timer) clearTimeout(timer)
 
+    if (immediate && !isExec) {
+      fn.apply(this, args)
+      isExec = true
+      return
+    }
+
     timer = setTimeout(() => {
       fn.apply(this, args)
       timer = null
+      isExec = false
     }, delay)
   }
 
   // 添加取消事件的功能
   _debounce.cancel = function () {
     if (timer) clearTimeout(timer)
+    timer = null
+    isExec = false
   }
 
   return _debounce
